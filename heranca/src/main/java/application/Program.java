509f4f1c883868de50b184c1bd0f6b84eac1715a
pauloscavallini <paulo.cavallini08@gmail.com>
2026-02-17 17:08:37 -0300

    inheritance

---
+++ heranca/src/main/java/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+import entities.BusinessAccount;
+
+public class Program {
+    public static void main(String[] args) {
+
+        BusinessAccount account = new BusinessAccount();
+
+    }
+}
