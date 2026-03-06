4c480ad52cfe51fabc259ca6fb3fb70443383f49
pauloscavallini <paulo.cavallini08@gmail.com>
2026-03-06 18:52:02 -0300

    Exercicio de fixacao exceptions

---
+++ exceptions-fixacao/src/main/java/application/Program.java
@@ -0,0 +1,40 @@
+package application;
+
+import model.entities.Account;
+import model.exceptions.WithdrawException;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter account data: ");
+        System.out.print("Number: ");
+        int number = sc.nextInt();
+
+        System.out.print("Holder: ");
+        sc.nextLine();
+        String holder = sc.nextLine();
+
+        System.out.print("Initial Balance: ");
+        double balance = sc.nextDouble();
+
+        System.out.print("Withdraw limit: ");
+        double withdrawLimit = sc.nextDouble();
+
+        Account account = new Account(number, holder, balance, withdrawLimit);
+
+        System.out.print("\nEnter the amount to withdraw: ");
+        double amount = sc.nextDouble();
+
+        try {
+            account.withdraw(amount);
+            System.out.println("New balance: " + account.getBalance());
+        } catch (WithdrawException e) {
+            System.out.println("Withdraw error: " + e.getMessage());
+        }
+
+        sc.close();
+    }
+}
