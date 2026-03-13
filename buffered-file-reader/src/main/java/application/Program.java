2006bceebe4200830b68e7992eb25a4a9a1a78d0
pauloscavallini <paulo.cavallini08@gmail.com>
2026-03-13 15:21:45 -0300

    FileWriter e BufferedWriter melhorados
    Aperfeiçoa estrutura try-catch

--- buffered-file-reader/src/main/java/application/Program.java
+++ buffered-file-reader/src/main/java/application/Program.java
@@ -8,34 +8,16 @@ public class Program {
     public static void main(String[] args) {
         String path = "C:\\temp\\in.txt";
 
-        FileReader fr = null;
-        BufferedReader br = null;
-        try {
-            fr = new FileReader(path);
-            br = new BufferedReader(fr); // BufferedReader é instanciado a partir de FileReader
-
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
             String line = br.readLine();
 
             while (line != null) {
                 System.out.println(line);
-                br.readLine();
+                line = br.readLine();
             }
         }
         catch (IOException e) {
             System.out.println("Error: " + e.getMessage());
-        }
-        finally {
-            try {
-                if (br != null) {
-                    br.close();
-                }
-                if (fr != null) {
-                    fr.close();
-                }
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
         }
     }
 }
